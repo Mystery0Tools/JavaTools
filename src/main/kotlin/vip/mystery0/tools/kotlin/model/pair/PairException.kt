7070d65e9f28788935e1442c5f4229d881cc0e4f
Mystery0 <mystery0dyl520@gmail.com>
2019-09-26 14:09:20 +0800

    fix: 优化代码

--- src/main/kotlin/vip/mystery0/tools/kotlin/model/pair/PairException.kt
+++ src/main/kotlin/vip/mystery0/tools/kotlin/model/pair/PairException.kt
@@ -6,16 +6,12 @@ package vip.mystery0.tools.kotlin.model.pair
  */
 open class PairException : RuntimeException {
     constructor()
-
     constructor(message: String) : super(message)
-
     constructor(message: String, cause: Throwable) : super(message, cause)
 }
 
 internal class NoSuchKeyException : PairException {
     constructor()
-
     constructor(message: String) : super(message)
-
     constructor(message: String, cause: Throwable) : super(message, cause)
 }
