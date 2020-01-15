b15c70151aa2b95465e488d2839a8448d906b4bf
Mystery0 <mystery0dyl520@gmail.com>
2020-01-15 13:41:06 +0800

    release: 1.2.5

--- buildSrc/src/main/kotlin/PublishConfig.kt
+++ buildSrc/src/main/kotlin/PublishConfig.kt
@@ -7,7 +7,7 @@ import java.io.File
 object PublishConfig {
     const val POM_GROUP_ID = "vip.mystery0.tools"
     const val POM_ARTIFACT_ID = "java.tools"
-    const val POM_VERSION = "1.2.4"
+    const val POM_VERSION = "1.2.5"
 
     const val POM_REPO_NAME = "tools"
     const val POM_USER_ORG = "mystery00"
