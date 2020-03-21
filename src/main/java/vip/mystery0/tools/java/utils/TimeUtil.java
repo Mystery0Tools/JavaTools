e937cfac61fb09a76966d90332875c7a2f0e20a6
Mystery0 <mystery0dyl520@gmail.com>
2020-03-21 21:43:20 +0800

    feat: 添加时长格式化的工具类

--- src/main/java/vip/mystery0/tools/java/utils/TimeUtil.java
+++ src/main/java/vip/mystery0/tools/java/utils/TimeUtil.java
@@ -1,63 +1,36 @@
 package vip.mystery0.tools.java.utils;
 
+import vip.mystery0.tools.kotlin.utils.TimeUnit;
 import vip.mystery0.tools.kotlin.utils.TimeUtilKt;
 
-import java.time.*;
-import java.time.format.DateTimeFormatter;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 /**
  * @author mystery0
  */
 public class TimeUtil {
-    public static String formatDate(LocalDateTime localDateTime) {
-        return TimeUtilKt.formatDate(localDateTime);
+    public static String formatTime(long timeInMillisecond) {
+        return TimeUtilKt.formatTime(timeInMillisecond, TimeUnit.MILLISECOND, TimeUnit.DAY);
     }
 
-    public static String formatDate(Instant instant) {
-        return TimeUtilKt.formatDate(instant);
+    public static String formatTime(long timeInMillisecond,
+                                    TimeUnit minTimeUnit,
+                                    TimeUnit maxTimeUnit) {
+        return TimeUtilKt.formatTime(timeInMillisecond, minTimeUnit, maxTimeUnit);
     }
 
-    public static String formatTime(LocalDateTime date) {
-        return TimeUtilKt.formatTime(date);
+    public static Timestamp toTimestamp(Instant instant) {
+        return TimeUtilKt.toTimestamp(instant, ZoneId.systemDefault());
     }
 
-    public static String formatTime(Instant instant) {
-        return TimeUtilKt.formatTime(instant);
+    public static Timestamp toTimestamp(Instant instant, ZoneId zoneId) {
+        return TimeUtilKt.toTimestamp(instant, zoneId);
     }
 
-    public static String formatDateTime(LocalDateTime date) {
-        return TimeUtilKt.formatDateTime(date);
-    }
-
-    public static String formatDateTime(Instant instant) {
-        return TimeUtilKt.formatDateTime(instant);
-    }
-
-    public static LocalDate parseDate(String string) {
-        return TimeUtilKt.parseDate(string);
-    }
-
-    public static LocalTime parseTime(String string) {
-        return TimeUtilKt.parseTime(string);
-    }
-
-    public static LocalDateTime parseDateTime(String string) {
-        return TimeUtilKt.parseDateTime(string);
-    }
-
-    public static String formatInstant(Instant instant, String pattern) {
-        return TimeUtilKt.formatInstant(instant, pattern);
-    }
-
-    public static String formatInstant(Instant instant, DateTimeFormatter dateTimeFormatter) {
-        return formatLocalDateTime(LocalDateTime.ofInstant(instant, ZoneId.systemDefault()), dateTimeFormatter);
-    }
-
-    public static String formatLocalDateTime(LocalDateTime localDateTime, String pattern) {
-        return TimeUtilKt.formatLocalDataTime(localDateTime, pattern);
-    }
-
-    public static String formatLocalDateTime(LocalDateTime localDateTime, DateTimeFormatter dateTimeFormatter) {
-        return dateTimeFormatter.format(localDateTime);
+    public static Timestamp toTimestamp(LocalDateTime localDateTime) {
+        return TimeUtilKt.toTimestamp(localDateTime);
     }
 }
--- src/main/kotlin/vip/mystery0/tools/kotlin/utils/TimeUtil.kt
+++ src/main/kotlin/vip/mystery0/tools/kotlin/utils/TimeUtil.kt
@@ -1,42 +1,88 @@
 package vip.mystery0.tools.kotlin.utils
 
 import java.sql.Timestamp
-import java.time.*
-import java.time.format.DateTimeFormatter
-import java.util.*
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 /**
  * 毫秒转化时分秒毫秒
  * @return
  */
-fun Long.formatTime(): String {
-    if (this <= 0) return "0毫秒"
+enum class TimeUnit(val level: Int, val unit: String, val interval: Int) {
+    MILLISECOND(0, "毫秒", 1000),
+    SECOND(1, "秒", 60),
+    MINUTE(2, "分", 60),
+    HOUR(3, "小时", 24),
+    DAY(4, "天", 1)
+}
+
+fun Long.formatTime(
+    minTimeUnit: TimeUnit = TimeUnit.MILLISECOND,
+    maxTimeUnit: TimeUnit = TimeUnit.DAY
+): String {
+    if (minTimeUnit.level > maxTimeUnit.level) {
+        //等级不正确，抛出异常
+        throw NumberFormatException("等级设置错误")
+    }
     val ss = 1000
     val mi = ss * 60
     val hh = mi * 60
     val dd = hh * 24
 
+    if (this <= 0) return "0${minTimeUnit.unit}"
+    if (maxTimeUnit == TimeUnit.MILLISECOND) return "$this${TimeUnit.MILLISECOND.unit}"
+
     val day = this / dd
-    val hour = (this - day * dd) / hh
-    val minute = (this - day * dd - hour * hh) / mi
-    val second = (this - day * dd - hour * hh - minute * mi) / ss
-    val milliSecond = this % ss
+    var hour = (this - day * dd) / hh
+    var minute = (this - day * dd - hour * hh) / mi
+    var second = (this - day * dd - hour * hh - minute * mi) / ss
+    var milliSecond = this % ss
 
     val sb = StringBuffer()
     if (day > 0) {
-        sb.append(day.toString() + "天")
+        val unit = TimeUnit.DAY
+        val nextUnit = TimeUnit.HOUR
+        if (maxTimeUnit.level < unit.level) {
+            hour += day * nextUnit.interval
+        } else {
+            sb.append(day).append(unit.unit)
+        }
+        if (minTimeUnit.level == unit.level) return sb.toString()
     }
     if (hour > 0) {
-        sb.append(hour.toString() + "小时")
+        val unit = TimeUnit.HOUR
+        val nextUnit = TimeUnit.MINUTE
+        if (maxTimeUnit.level < unit.level) {
+            minute += hour * nextUnit.interval
+        } else {
+            sb.append(hour).append(unit.unit)
+        }
+        if (minTimeUnit.level == unit.level) return sb.toString()
     }
     if (minute > 0) {
-        sb.append(minute.toString() + "分")
+        val unit = TimeUnit.MINUTE
+        val nextUnit = TimeUnit.SECOND
+        if (maxTimeUnit.level < unit.level) {
+            second += minute * nextUnit.interval
+        } else {
+            sb.append(minute).append(unit.unit)
+        }
+        if (minTimeUnit.level == unit.level) return sb.toString()
     }
     if (second > 0) {
-        sb.append(second.toString() + "秒")
+        val unit = TimeUnit.SECOND
+        val nextUnit = TimeUnit.MILLISECOND
+        if (maxTimeUnit.level < unit.level) {
+            milliSecond += second * nextUnit.interval
+        } else {
+            sb.append(second).append(unit.unit)
+        }
+        if (minTimeUnit.level == unit.level) return sb.toString()
     }
     if (milliSecond > 0) {
-        sb.append(milliSecond.toString() + "毫秒")
+        val unit = TimeUnit.MILLISECOND
+        sb.append(milliSecond).append(unit.unit)
     }
     return sb.toString()
 }
