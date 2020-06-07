30960b17f1ec96918789047a4842ccfc568123f6
Mystery0 <mystery0dyl520@gmail.com>
2020-06-07 17:56:31 +0800

    fix: Json库添加设置ObjectMapper的方法

--- src/main/java/vip/mystery0/tools/java/factory/JsonFactory.java
+++ src/main/java/vip/mystery0/tools/java/factory/JsonFactory.java
@@ -1,6 +1,7 @@
 package vip.mystery0.tools.java.factory;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import vip.mystery0.tools.kotlin.factory.JsonFactoryKt;
 
 import java.lang.reflect.Type;
@@ -9,6 +10,13 @@ import java.lang.reflect.Type;
  * @author mystery0
  */
 public class JsonFactory {
+    public static ObjectMapper getObjectMapper() {
+        return vip.mystery0.tools.kotlin.factory.JsonFactory.INSTANCE.getObjectMapper();
+    }
+
+    public static void setObjectMapper(ObjectMapper objectMapper) {
+        vip.mystery0.tools.kotlin.factory.JsonFactory.INSTANCE.setObjectMapper(objectMapper);
+    }
 
     public static <T> T fromJson(String json, Class<T> clazz) {
         return JsonFactoryKt.fromJson(json, clazz);
